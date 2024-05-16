63bdaf1bc6cac86f12353d36b4d5a9839decb5f5
KIMGWANYEONG <pipe0youngk@gmail.com>
2024-05-16 13:46:07 +0900

    Home chatlist

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FiSearch } from "react-icons/fi";
 
 const Home = () => {
   const [content, setContent] = useState("");
+  const [chatlist, setChatlist] = useState();
 
   const onSubmitChat = async (e) => {
     try {
@@ -30,14 +31,21 @@ const Home = () => {
         }
       );
 
-      console.log(response);
+      setChatlist({
+        question: content,
+        answer: response.data.choices[0].message.content,
+      });
     } catch (error) {
       console.error(error);
     }
   };
 
+  useEffect(() => {
+    console.log(chatlist);
+  }, [chatlist]);
+
   return (
-    <div className="mt-8 flex justify-center">
+    <div className="mt-8 flex flex-col items-center">
       <form className="flex" onSubmit={onSubmitChat}>
         <input
           className="text-2xl p-2 focus:outline-none rounded-lg border-2 border-pink-200 focus:border-pink-400"
@@ -53,6 +61,12 @@ const Home = () => {
           검색
         </button>
       </form>
+      <ul className="mt-8 px-4 flex flex-col gap-4">
+        <li className="bg-pink-50 p-4 rounded-md text-lg shadow-md shadow-pink-50">
+          <div className="mb-2 font-semibold">Q. {chatlist?.question}</div>
+          <div>A. {chatlist?.answer}</div>
+        </li>
+      </ul>
     </div>
   );
 };
