b4bee8d3ce2e510b9a08c7d5d178bfec073cfc03
KIMGWANYEONG <pipe0youngk@gmail.com>
2024-05-16 16:02:05 +0900

    fin

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,21 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import ChatlistCard from "../components/ChatlistCard";
+import { CgOpenCollective } from "react-icons/cg";
 
 const Home = () => {
   const [content, setContent] = useState("");
-  const [chatlist, setChatlist] = useState();
+  const [chatlist, setChatlist] = useState([]);
+  const [isLoading, setLoading] = useState(false);
 
   const onSubmitChat = async (e) => {
     try {
       e.preventDefault();
 
       if (!content) return;
+
+      setLoading(true);
 
       const response = await axios.post(
         "https://api.openai.com/v1/chat/completions",
@@ -31,18 +36,32 @@ const Home = () => {
         }
       );
 
-      setChatlist({
+      const newChat = {
         question: content,
         answer: response.data.choices[0].message.content,
-      });
+      };
+
+      let savedChatlist = localStorage.getItem("savedChatlist");
+
+      if (!savedChatlist) {
+        savedChatlist = [];
+      } else {
+        savedChatlist = JSON.parse(savedChatlist);
+      }
+
+      savedChatlist.push(newChat);
+
+      localStorage.setItem("savedChatlist", JSON.stringify(savedChatlist));
+
+      setChatlist([newChat, ...chatlist]);
+
+      setLoading(false);
     } catch (error) {
       console.error(error);
+
+      setLoading(false);
     }
   };
-
-  useEffect(() => {
-    console.log(chatlist);
-  }, [chatlist]);
 
   return (
     <div className="mt-8 flex flex-col items-center">
@@ -51,21 +70,26 @@ const Home = () => {
           className="text-2xl p-2 focus:outline-none rounded-lg border-2 border-pink-200 focus:border-pink-400"
           type="text"
           value={content}
+          disabled={isLoading}
           onChange={(e) => setContent(e.target.value)}
         />
         <button
           className="ml-4 flex items-center bg-pink-400 text-2xl px-4 py-[10px] rounded-full shadow-md shadow-pink-200 hover:bg-pink-500"
           type="submit"
+          disabled={isLoading}
         >
-          <FiSearch className="mr-2" />
+          {isLoading ? (
+            <CgOpenCollective className="mr-2 animate-spin-slow" />
+          ) : (
+            <FiSearch className="mr-2" />
+          )}
           검색
         </button>
       </form>
       <ul className="mt-8 px-4 flex flex-col gap-4">
-        <li className="bg-pink-50 p-4 rounded-md text-lg shadow-md shadow-pink-50">
-          <div className="mb-2 font-semibold">Q. {chatlist?.question}</div>
-          <div>A. {chatlist?.answer}</div>
-        </li>
+        {chatlist.map((v, i) => (
+          <ChatlistCard key={i} question={v.question} answer={v.answer} />
+        ))}
       </ul>
     </div>
   );
